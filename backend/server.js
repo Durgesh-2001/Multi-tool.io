1756db244294b8f84ab79402bc5ca98f40968ef2
Durgesh-jay <dj26112001@gmail.com>
2025-07-30 12:22:01 +0530

    Updated CORS for Frontend

--- backend/server.js
+++ backend/server.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 const allowedOrigins = [
   'https://multi-tooldot-io.vercel.app',
   'http://localhost:5173',
-  'http://localhost:74'
+  'http://localhost:5174'
 ];
 
 const corsOptions = {
